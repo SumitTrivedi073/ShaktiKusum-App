396522b4f36ae184ed5a13e6fcea658d826801c5
shakti <android@shaktipumps.com>
2023-06-28 16:14:54 +0530

    1) change controller ID

--- app/src/main/java/activity/InstallationInitial.java
+++ app/src/main/java/activity/InstallationInitial.java
@@ -1490,7 +1490,7 @@ public class InstallationInitial extends BaseActivity {
         inst_pump_ser.setText(pump);
 
         WebURL.mDEvice_Number_CHECK = controller;
-        inst_controller_ser.setText("7F-0135-0-13-06-23");
+        inst_controller_ser.setText(controller);
 
         if (!TextUtils.isEmpty(installationBean.getSimoprator())) {
             spinner_simoprator.setSelection(db.getPosition(spinner_simoprator, installationBean.getSimoprator()));
